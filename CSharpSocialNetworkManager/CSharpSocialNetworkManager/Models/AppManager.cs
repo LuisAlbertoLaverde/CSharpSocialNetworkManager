d70bc28037e3bb6a0d8f5aa8988c4ee7087e3cf1
Alberto <unumnio@gmail.com>
2022-10-06 22:28:14 -0500

    created class AppManager whit atributte AppTittle and class User whit atributes (Name, Email), created objets App and User

---
+++ CSharpSocialNetworkManager/CSharpSocialNetworkManager/Models/AppManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSocialNetworkManager.Models
+{
+    class AppManager
+    {
+        public string AppTittle { get; set; }
+
+        public AppManager()
+        {
+            AppTittle = "Adminsitrador de redes sociales";
+        }
+    }
+}
---
+++ CSharpSocialNetworkManager/CSharpSocialNetworkManager/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CSharpSocialNetworkManager.Models;
+
+AppManager App = new AppManager();
+Console.WriteLine($"Bienvenido a {App.AppTittle}");
+
+User User = new User(); 
+
+Console.WriteLine("Por favor ingrese su nombre");
+User.Name = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su correo electronico");
+User.Email = Console.ReadLine();
+
+//name= User.Name;
+//email= User.Email;
+
+Console.WriteLine("Sus datos son:");
+Console.WriteLine($"Nombre: {User.Name}");
+Console.WriteLine($"Correo electronico: {User.Email}");
